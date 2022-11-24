1d0d5920b4522e39eea382a91763826bbc90692c
Odilf <odysseas.maheras@gmail.com>
2022-11-24 19:42:22 +0100

    added a projects page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 		<path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
 	</svg>
 
-	<p>
+	<h1>
 		Website still in construction!
-	</p>
+	</h1>
 
 	<p>	
 		In the meantime, check out my <a href='/projects'> projects </a> or my <a href='https://github.com/Odilf' target="_blank" rel="noreferrer"> GitHub </a>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,100 @@
+<h1> Projects </h1>
+<p>
+	Here are some of the things I've done over the years: 
+</p>
+
+<ul>
+	<li class="wip"> 
+		<a href="https://pistoltimer.com/"> Pistol</a>:
+		A modern speedcubing timer.
+	</li>
+
+	<li> 
+		<a href="https://famm.odilf.com/"> FAMM.com</a>:
+		The official fanpage for fans of Maggie!
+	</li>
+
+	<li class="wip"> 
+		<a href="https://github/Odilf/Barbarosa.com/"> Barbarosa</a>:
+		A Rubik's cube solver, written in Julia.
+	</li>
+	
+	<li> 
+		<a href="https://github/Odilf/Dveco/"> Dveco</a>:
+		A recursive lossy image compressor based on standard deviation, clearly worse than JPEG 
+	</li>
+
+	<li> 
+		<a href="https://github/Odilf/spasmodic-consonizer/"> Spasmodic Consonizer</a>:
+		A fuzzy consonance analyser
+	</li>
+
+	<li> 
+		<a href="https://rummy.odilf.com/"> Rummy</a>:
+		An online version of the board game, Rummy.
+	</li>
+
+	<li> 
+		<a href="https://github.com/Odilf/daedalus"> Daedalus</a>:
+		A game about a labyrinth. Basically, an excuse to make a 3D renderer in x86 assembly
+	</li>
+
+	<li> 
+		<a href="https://github.com/Odilf/metadata-transferer"> Metadata transferer</a>:
+		A simple CLI tool to transfer metadata between files.
+	</li>
+
+	<li> 
+		<a href="https://github.com/Odilf/Chessagon"> CHESSAGON</a>:
+		Hexagonal chess!
+	</li>
+
+	<li> 
+		<a href="https://github.com/Odilf/bottom-station"> Bottom station</a>:
+		Culo simulator and analizer. 
+	</li>
+
+	<li> 
+		<a href="https://minigames.odilf.com"> Minigames</a>:
+		An assortment of minigames
+	</li>
+	
+	<li> 
+		<a href="https://puta.odilf.com/">  PCnv</a>:
+		Encode your messages with just a single word. 
+	</li>
+
+	<li> 
+		<a href="https://baselga.odilf.com/"> Baselga timer</a>: 
+		To correct time estimates of Sir Baselga
+	</li>
+</ul>
+
+<p style='margin-top: 3em; opacity: 50%;'>
+	..or you could just directly check out my <a href="https://github.com/Odilf"> GitHub </a>
+</p>
+
+	
+<style>
+	h1 {
+		margin-block: 1em;
+	}
+
+	li {
+		padding: 6px 3px;
+		border-radius: 1em;
+	}
+
+	a {
+		transition: 200ms color ease;
+	}
+	
+	a:hover {
+		color: var(--primary-content)
+	}
+
+	.wip::after {
+		content: " (in progress)";
+		opacity: 30%;
+	}
+</style>
