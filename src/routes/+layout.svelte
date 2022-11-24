d952ff14b0e04a46586c1d4170b4735273a573c0
Odilf <odysseas.maheras@gmail.com>
2022-11-24 18:48:27 +0100

    updated. a lot

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import "../styles.css"
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		max-width: 820px;
+		margin-inline: auto;
+		padding: 12px;
+
+		height: 100%;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-/* jshint ignore:start */
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,9 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: vercel()
+		adapter: adapter()
 	}
 };
 
