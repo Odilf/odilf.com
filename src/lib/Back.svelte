c3eb561a3fb7064a1bc477d520c72826333bb6f8
Odilf <odysseas.maheras@gmail.com>
2022-11-27 00:51:37 +0100

    cleanup some comments

--- src/lib/Back.svelte
+++ src/lib/Back.svelte
@@ -6,14 +6,4 @@
 	button:hover {
 		text-decoration: underline;
 	}
-/* 
-	button {
-		border-radius: var(--rounded);
-		text-align: left;
-
-		opacity: 90%;
-		z-index: 10;
-		background-color: transparent;
-		backdrop-filter: blur(100px);
-	} */
 </style>
